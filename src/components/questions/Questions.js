7d728e9c6387f8ee0620db2440ffec945a7e2b62
Badbreaddead <Pavlovandrew@mail.ru>
2017-05-11 17:51:24 +0800

    Table initialized

--- src/components/questions/Questions.js
+++ src/components/questions/Questions.js
@@ -1,13 +1,14 @@
 import React, {Component, PropTypes} from 'react';
 import { Link } from 'react-router'
 import { Col, Row } from 'antd';
+import Table from './Table';
 
 class MainPage extends Component {
 
     render() {
         return (
             <div>
-                Main Page
+                <Table />
             </div>
         );
     }
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import BaseComponent from '../components/BaseComponent';
 import NotFoundComponent from '../components/common/NotFoundComponent';
 import WrappedLogin from '../components/login/Login';
-import MainPage from '../components/main/MainPage';
+import Questions from '../components/questions/Questions';
 
 export const urls = {
     index: {
@@ -18,7 +18,7 @@ export const urls = {
 export default (
     <Route>
         <Route path={urls.index.path} component={BaseComponent}>
-            <IndexRoute component={MainPage} />
+            <IndexRoute component={Questions} />
         </Route>
         <Route path={urls.login.path} component={WrappedLogin} />
         <Route path="*" component={NotFoundComponent} />
