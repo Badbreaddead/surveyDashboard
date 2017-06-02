7d7b6a7e40153cf60776222b6baf947e9b88b470
Badbreaddead <Pavlovandrew@mail.ru>
2017-06-02 22:39:29 +0800

    SSL HTTPS

--- server.production.js
+++ server.production.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var https = require('https');
+const express = require('express');
+const app = express();
+const path = require('path');
+const https = require('https');
+const fs = require('fs');
 
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
+const privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+const certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
 
 const port = process.env.PORT || 3000;
 
