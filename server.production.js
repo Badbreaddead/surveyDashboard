779ff2aaf7e8beabb1343bf2eacc912e4ca21684
Badbreaddead <Pavlovandrew@mail.ru>
2017-06-02 22:33:35 +0800

    SSL HTTPS

--- server.production.js
+++ server.production.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var https = require('https');
 
-app.set('port', (process.env.PORT || 3000));
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+const port = process.env.PORT || 3000;
+
+app.server = http.createServer(app);
+app.httpsServer = https.createServer(credentials, app);
 
 app.use(express.static(__dirname + '/dist'));
 app.get('*', function(req, res) {
   res.sendFile(path.resolve(__dirname, './dist/index.html'));
 });
 
-app.listen(app.get('port'), function() {
+app.listen(port, function() {
     console.log('Server running on port ', app.get('port'));
 });
