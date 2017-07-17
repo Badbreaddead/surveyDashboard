ac555b3c09bdc4cc5989434794a82e24e3472d2a
Badbreaddead <Pavlovandrew@mail.ru>
2017-07-17 14:25:03 +0800

    Facebook bot server config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export default {
-	//https://farzad-bot-api.herokuapp.com/
-	//http://174.138.52.48:5000/
-  baseUrl: 'http://localhost:5000/'
-  // baseUrl: 'http://174.138.52.48:5000/'
+  // baseUrl: 'http://localhost:5000/'
+  // baseUrl: 'https://farzad-bot-api.herokuapp.com/'
+  baseUrl: 'http://174.138.52.48:5000/'
 };
