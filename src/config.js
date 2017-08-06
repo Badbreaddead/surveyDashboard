20feaf74bdc98e6ce53bf0ab1e5ba0c209067d4c
Badbreaddead <Pavlovandrew@mail.ru>
2017-08-06 12:36:58 +0800

    https

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   // baseUrl: 'http://localhost:5000/'
   // baseUrl: 'https://farzad-bot-api.herokuapp.com/'
-  baseUrl: 'https://174.138.52.48/'
+  baseUrl: 'https://coinsurvey.me/'
 };
